503f013eafab1595722eb15ca3f09254747ba378
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 16:47:15 -0700

    added import for home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from 'react-router-dom';
 import Layout from './Page/Layout';
+import Home from './Home/Home';
 import List from './List/List';
 import About from './About/About';
 
@@ -13,8 +14,10 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="list" element={<List />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
     </Router>
