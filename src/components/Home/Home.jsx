a8a942f7d0e37d5036a24078d987e8a620098243
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 16:51:33 -0700

    added styling and wording to home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import styles from './Home.css';
+
+export default function Home() {
+  return (
+    <div className={styles.Home}>
+      <h1>Home Sweet Home</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+        dolorum iusto esse aperiam ullam magni sint expedita, magnam maxime
+        provident aut corrupti cum voluptates culpa ab eveniet soluta.
+        Cupiditate, tenetur!
+      </p>
+    </div>
+  );
+}
