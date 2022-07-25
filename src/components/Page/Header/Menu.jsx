fe0898b378ca3217597289b0776d8beb41fd56b0
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 16:10:54 -0700

    wrote out Menu.jsx

---
+++ src/components/Page/Header/Menu.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Navigation from './Navigation.jsx';
+import styles from './Header.css';
+import classNames from 'classnames';
+
+export default function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const className = classNames(styles.Menu, {
+    [styles.isOpen]: isOpen,
+  });
+
+  const handleClick = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  return (
+    <button className={className} onclick={handleClick}>
+      <div className={styles.MenuContainer}>
+        <Navigation />
+      </div>
+    </button>
+  );
+}
