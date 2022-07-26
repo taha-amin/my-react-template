d61a4e474a29bb40da4b22c4e51a8a7f71c17842
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 17:20:30 -0700

    added more to list.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,6 +3,8 @@ import styles from './List.css';
 export default function List() {
   return (
     <div className={styles.List}>
+      <h1>This is a List Page</h1>
+      <br />
       <li>first item</li>
       <li>second item</li>
       <li>third item</li>
