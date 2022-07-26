282fb8f0668f3266b836c3581f4cda1f81a7323e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 17:00:27 -0700

    styled list page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,11 @@
+import styles from './List.css';
+
 export default function List() {
-  return <p>I am the list page</p>;
+  return (
+    <div className={styles.List}>
+      <li>first item</li>
+      <li>second item</li>
+      <li>third item</li>
+    </div>
+  );
 }
