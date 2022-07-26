6957b1db116eb2c37243ca7742dfeb84bb0ec3b7
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-25 17:03:48 -0700

    styled about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,14 @@
+import styles from './About.css';
+
 export default function About() {
-  return <p>I am the About page</p>;
+  return (
+    <div className={styles.About}>
+      <h1>About This Assignment</h1>
+      <br />
+      <p>
+        This assignment is a simple React app that demonstrates the use of React
+        Router and React Router Outlet.
+      </p>
+    </div>
+  );
 }
