8871817e420b0b052280f735c111b41b09a9808e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-29 09:12:01 -0700

    removed image from home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,9 @@
 import styles from './Home.css';
-import PokemonCenterImage from '/Users/mohamedamin/Desktop/alchemy/Module-4/Week-1/my-react-template/public/pokemon-center.jpeg';
 
 export default function Home() {
   return (
-    <div
-      className={styles.Home}
-      style={{ backgroundImage: 'url("pokemon-center.jpeg")' }}
-    >
+    <div className={styles.Home}>
       <h1>Alchemy Pokemon Center</h1>
-      {/* <img src={PokemonCenterImage} alt="pokemon center" /> */}
       <br />
       <p>
         Welcome to the Alchemy Pokemon Center! Take a rest from all your travels
