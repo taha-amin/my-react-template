f21da82c9a5df31af50bd182874f3300cd8d1a49
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-05 09:36:04 -0700

    Edited and styled about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,48 +3,66 @@ import {
   SelectControl,
   TextAreaControl,
   CheckboxControl,
-  FormButton,
-  Fieldset,
-} from '../Forms/FormControls';
+  OptionGroupControl,
+  CheckboxOption,
+  RadioOption,
+  FormButtonControl,
+} from '../Forms/FormControls.jsx';
+
 import styles from './About.css';
 
 export default function About() {
   return (
     <section className={styles.About}>
       <form>
-        <Fieldset legend="About Form">
-          <InputControl
-            label="email"
-            name="email"
-            placeholder="enter your email"
-            type="email"
-            required
-          />
+        <InputControl
+          label="email"
+          name="email"
+          placeholder="enter your email"
+          type="email"
+          required
+        />
 
-          <InputControl
-            className={styles.PasswordControl}
-            label="password"
-            name="password"
-            placeholder="enter your password"
-            type="password"
-            required
-          />
-        </Fieldset>
+        <InputControl
+          className={styles.PasswordControl}
+          label="password"
+          name="password"
+          type="password"
+          placeholder="choose a password"
+          required
+        />
 
-        <SelectControl label="Select your favorite food">
-          <option>Select</option>
-          <option>Pasta</option>
-          <option>Pizza</option>
-          <option>Burrito</option>
-          <option>Sushi</option>
-          <option>Burger</option>
+        <SelectControl label="type">
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
         </SelectControl>
 
-        <CheckboxControl label="Are you Human?" text="Yes" />
+        <CheckboxControl label="Easy Mode?" text="Yes" />
 
-        <TextAreaControl label="Bio" placeholder="tell us about yourself" />
+        <OptionGroupControl label="Check all that apply" size="125px">
+          <CheckboxOption text="Monday" />
+          <CheckboxOption text="Tuesday" />
+          <CheckboxOption text="Thursday" />
+          <CheckboxOption text="Wednesday" />
+          <CheckboxOption text="Friday" />
+          <CheckboxOption text="Saturday" />
+          <CheckboxOption text="Sunday" />
+        </OptionGroupControl>
 
-        <FormButton>Submit</FormButton>
+        <OptionGroupControl name="pet" label="Favorite Pet" size="95px">
+          <RadioOption value={1} text="dog" />
+          <RadioOption value={2} text="cat" />
+          <RadioOption value={3} text="bird" />
+          <RadioOption value={4} text="fish" />
+          <RadioOption value={5} text="lizard" />
+          <RadioOption value={6} text="snake" />
+          <RadioOption value={7} text="llama" />
+        </OptionGroupControl>
+
+        <TextAreaControl label="bio" placeholder="tell us about yourself" />
+
+        <FormButtonControl>Submit</FormButtonControl>
       </form>
     </section>
   );
